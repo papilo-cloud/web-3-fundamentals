27fb74603008389edc721e7025b2a9408f763d2e
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-18 00:03:08 +0100

    add transaction

---
+++ build-a-miner/scripts/mempool.js
@@ -0,0 +1,24 @@
+import sha256 from 'crypto-js/sha256.js';
+const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    // TODO: add transaction to mempool
+    mempool.push(transaction)
+}
+
+function mine() {
+    // TODO: mine a block
+}
+
+export {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction, 
+    mine, 
+    blocks,
+    mempool
+};
