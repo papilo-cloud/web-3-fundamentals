0063a5001038df0aad8bcf0f0d2deff34bb1767d
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-19 23:59:58 +0100

    test for validating the chain

---
+++ blockchain-data-structure/tests/6-validationTest.js
@@ -0,0 +1,38 @@
+import { assert } from "chai";
+import BlockChain from "../scripts/3-blockchain.js";
+import Block from "../scripts/blocks.js";
+import SHA256 from 'crypto-js/sha256.js';
+
+describe('Blockchain', function() {
+  let blockchain;
+  beforeEach(() => {
+    blockchain = new BlockChain();
+    blockchain.addBlock(new Block("Dan"));
+    blockchain.addBlock(new Block("Peter"));
+    blockchain.addBlock(new Block("James"));
+  });
+  
+  it('should be considered valid', function() {
+    assert(blockchain.isValid());
+  });
+
+  describe('tampering with a previousHash', function() {
+    beforeEach(() => {
+      blockchain.chain[1].previousHash = SHA256("gibberish");
+    });
+
+    it('should not be considered valid', function() {
+      assert(!blockchain.isValid());
+    });
+  });
+  
+  describe('tampering with data', function() {
+    beforeEach(() => {
+      blockchain.chain[0].data = "Something Else";
+    });
+
+    it('should not be considered valid', function() {
+      assert(!blockchain.isValid());
+    });
+  });
+});
