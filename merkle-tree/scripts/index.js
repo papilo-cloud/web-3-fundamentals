2c72e5935e200c6c9cc946a8ab6d67f801183e54
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-23 01:58:00 +0100

    multiple trees

--- merkle-tree/scripts/index.js
+++ merkle-tree/scripts/index.js
@@ -4,7 +4,16 @@ class MerkleTree {
         this.concat = concat;
     }
     getRoot(leaves = this.leaves) {
-        return this.concat(leaves[0], leaves[1])
+        if (leaves.length == 1){
+            return leaves[0]
+        }
+        let root = []
+        for (let i = 0; i < leaves.length; i += 2){
+            const left = leaves[i]
+            const right = leaves[i + 1]
+            root.push(this.concat(left, right))
+        }
+        return this.getRoot(root)
     }
 }
 
