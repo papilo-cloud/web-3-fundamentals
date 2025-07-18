8fec76e701dc1c3d4d1d115d9f449166aa1aea26
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-18 22:55:15 +0100

    proposal storage

---
+++ Governance/contracts/Voting1.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+
+    Proposal[] public proposals;
+
+    function newProposal(address _addr, bytes calldata _data) external {
+        Proposal memory proposal = Proposal(_addr, _data, 0, 0);
+        proposals.push(proposal);
+    }
+    
+}
